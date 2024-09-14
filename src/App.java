4c94a1619cb3d641394dcd89cb063675ca1f0c58
joaooliveira <joao.oliveira0117@hotmail.com>
2024-09-14 17:52:36 -0300

    fixes

--- src/App.java
+++ src/App.java
@@ -15,7 +15,7 @@ import java.nio.file.Paths;
 public class App {
     public static void main(String[] args) throws Exception {
         HashTable hashTable = new HashTable();
-        
+
         Path publicPath = Paths.get("../public").toAbsolutePath().normalize();
         String filepath = publicPath.toString() + "/text.txt";
 
@@ -29,9 +29,25 @@ public class App {
         int emojiRange = 0x1F64F - 0x1F600;
 
         for(String word : words) {
-            if(!word.isBlank()) {
-                int codePoint = baseEmojiNumber + ( word.chars().sum() % (emojiRange + 1) );
+            if (!word.isBlank()) {
+                int i = 0;
+                int codePoint = baseEmojiNumber + (word.chars().sum() % (emojiRange + 1));
                 String emoji = Character.toString(codePoint);
+        
+                if (hashTable.get(emoji).equals(word)) {
+                    sb.append(emoji);
+                    continue;
+                }
+                
+                while (hashTable.get(emoji) != null && !hashTable.get(emoji).isEmpty() && !hashTable.get(emoji).isBlank()) {
+                    i++;
+                    if (i > emojiRange) {
+                        i = 0;
+                    }
+                    codePoint = baseEmojiNumber + i;
+                    emoji = Character.toString(codePoint);
+                }
+
                 hashTable.set(emoji, word);
                 sb.append(emoji);
             }
